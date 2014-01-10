ff472cd9432db7a996060b6dda5f4236a1bdd3ff
Wallace Daniel <wdaniel@redhat.com>
2014-01-10 16:08:48 -0500

    Path testing

--- wsgi/views.py
+++ wsgi/views.py
@@ -17,7 +17,7 @@ During server init, here is what should happen:
 # Load the database and it's hosts
 #dbFile = "/home/wallace/cases/01002915/sosreport-LogCollector-20131223183303/database/sos_pgdump.tar"
 #dbFile = "/home/wallace/cases/00945926/LogCollector/database/sos_pgdump.tar"
-dbFile = "data/sos_pgdump.tar"
+dbFile = "/data/sos_pgdump.tar"
 database = Database(dbFile)
 
 # get other lists of information for reference
